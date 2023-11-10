a1dcc737da677505c940ce47084fabe6ca1976d3
Rahul Sharma <rsshrma92@hotmail.com>
2023-11-10 19:03:07 +0530

    3 star

---
+++ Exercises/pdf_rect_length.py
@@ -0,0 +1,8 @@
+arr = [1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7]
+str1 = "zaba"
+list1 = []
+for i in str1:
+    list1.append(arr[ord(i)-97])
+
+print( max(list1)*len(list1))
+
